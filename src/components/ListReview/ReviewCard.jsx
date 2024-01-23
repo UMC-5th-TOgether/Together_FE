7bed9f9ddf1418ae77e50c4cb1463969b2659620
paul0755 <100present@naver.com>
2024-01-23 23:08:13 +0900

    #301 Feat: API받을 준비중(1)

--- src/components/ListReview/ReviewCard.jsx
+++ src/components/ListReview/ReviewCard.jsx
@@ -1,32 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ReviewCard.css";
 import profileImg from "../../assets/profile.png";
 import fullStarImg from "../../assets/fullstar.png";
+import emptyStarImg from "../../assets/emptystar.png";
 //UserIconImg server에게 get,post 요청 해서 이미지 받아야함.
 
 export default function ReviewCard() {
+  const ReviewsData = [
+    {
+      pimg: profileImg,
+      alt: "prfileImg",
+      name: "hihihi",
+      tags: ["#줌 스터디", "#공부", "#카페"],
+      average: 5,
+      review:
+        "이런 서비스를 쓸 때 어떤 사람들을 만나게 될지 몰라서 처음에는 솔직히 걱정스러웠지만 이젠 부담감 없이 사용할 수 있어요.",
+    },
+  ];
+
+  const [Slideindex, setSlideindex] = useState(0);
+
+  const ratingToPercent = {
+    width: `${ReviewsData[Slideindex].average * 15.5 + 5}%`,
+  };
   return (
     <div className="ReviewBox">
       <div className="Contents">
         <div className="UserBox">
-          <img src={profileImg} alt="profile" />
+          <img
+            src={ReviewsData[Slideindex].pimg}
+            alt={ReviewsData[Slideindex].alt}
+          />
           <div className="UserDetailBox">
-            <div className="UserName">naningu</div>
+            <div className="UserName">{ReviewsData[Slideindex].name}</div>
             <div className="CategoryBox">
-              <div className="CategoryHash">#에스파</div>
-              <div className="CategoryHash">#에스파</div>
-              <div className="CategoryHash">#에스파</div>
+              {ReviewsData[Slideindex].tags.map((tag, idx) => (
+                <div className="CategoryHash" key={idx}>
+                  {tag}
+                </div>
+              ))}
             </div>
             <div className="StarBox">
-              <img src={fullStarImg} alt="fullstar" />
-              <div className="Point">5</div>
+              <div className="star-ratings-fill" style={ratingToPercent}>
+                <img src={fullStarImg} alt="fullstar" />
+                <img src={fullStarImg} alt="fullstar" />
+                <img src={fullStarImg} alt="fullstar" />
+                <img src={fullStarImg} alt="fullstar" />
+                <img src={fullStarImg} alt="fullstar" />
+              </div>
+              <div className="star-rating-base">
+                <img src={emptyStarImg} alt="emptystar" />
+                <img src={emptyStarImg} alt="emptystar" />
+                <img src={emptyStarImg} alt="emptystar" />
+                <img src={emptyStarImg} alt="emptystar" />
+                <img src={emptyStarImg} alt="emptystar" />
+              </div>
+              <div className="Point">{ReviewsData[Slideindex].average}</div>
             </div>
           </div>
         </div>
-        <div className="UserReviewBox">
-          처음에는 어색할까봐 걱정했지만, 공통된 취향과 관심사를 가진 사람들과
-          매칭되어 함께 다양한 활동을 즐길 수 있었어요!
-        </div>
+        <div className="UserReviewBox">{ReviewsData[Slideindex].review}</div>
       </div>
     </div>
   );
