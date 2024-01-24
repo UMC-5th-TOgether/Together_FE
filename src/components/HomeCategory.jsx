45f8ebb5671ef6859521b670ebefc27764b77a34
angoroa <aaa000117@naver.com>
2024-01-25 02:54:42 +0900

    img 추가(category,post,review)

--- src/components/HomeCategory.jsx
+++ src/components/HomeCategory.jsx
@@ -9,7 +9,14 @@ export default function HomeCategory() {
         <div className= "Category-Info">
     
             <br/>
-            <h3 style={{ fontFamily: '"Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif', fontWeight: 'bold',fontSize: '0.8rem', color:'#B3B3B3', display:'flex',justifyContent:'center'}}>카테고리 빠른 이동</h3>
+            <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+              <img
+                style={{ width: "92px", height: "19px" }}
+                src="/Category.png"
+                alt="로고"
+              />
+            </div>
+           
             <br/>
             <h1 style={{ fontFamily: '"Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif', fontWeight: 'bold',fontSize: '1.4rem' }}>
               4가지 카테고리로 원하는 매칭을 찾아보세요 !
@@ -18,22 +25,58 @@ export default function HomeCategory() {
           <div className="Category-Container-Wrapper">  
             <div className = "Category-Container">
               <div className="Category-Wrap">
-                <Link className="Category-Button" to ="/ctgmeal">01 식사</Link>
+                <Link className="Category-Button" to ="/ctgmeal">식사 
+                  <div>
+                      <img
+                        src="/Arrow.png"
+                        alt="로고"
+                      />
+                  </div>
+                </Link>
               </div>
               <div className="Category-Wrap">
-                <Link className="Category-Button" to ="/ctgexercise">02 운동</Link>
+                <Link className="Category-Button" to ="/ctgexercise">운동
+                  <div>
+                      <img
+                        src="/Arrow.png"
+                        alt="로고"
+                      />
+                  </div>                
+                </Link>
               </div>
               <div className="Category-Wrap">
-                <Link className="Category-Button" to ="/ctgconcert">03 공연</Link>
+                <Link className="Category-Button" to ="/ctgconcert">공연
+                  <div>
+                      <img
+                        src="/Arrow.png"
+                        alt="로고"
+                      />
+                  </div>   
+                </Link>
               </div>
               <div className="Category-Wrap">
-                <Link className="Category-Button" to ="/ctghobby">04 취미</Link>
+                <Link className="Category-Button" to ="/ctghobby">취미
+                  <div>
+                      <img
+                        src="/Arrow.png"
+                        alt="로고"
+                      />
+                  </div>   
+                </Link>
               </div>
             </div>
           </div>
           <br/>
           <br/>
           <br/>
+          <br/>
+          <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+              <img
+                style={{ width: "77px", height: "19px" }}
+                src="/Posting.png"
+                alt="로고"
+              />
+          </div>
           <br/>
           <h1 style={{ fontFamily: '"Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif', fontWeight: 'bold', fontSize: '1.4rem' }}>
             한눈에 보는 카테고리별 인기 포스트
