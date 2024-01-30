607c1e211156e6a4fa827fe57a33cd7a4a35b716
paul0755 <100present@naver.com>
2024-01-30 17:57:34 +0900

    #302 Feat: Friend-Matching UI추가

--- src/components/ListReview/ReviewCard.jsx
+++ src/components/ListReview/ReviewCard.jsx
@@ -8,12 +8,12 @@ import emptyStarImg from "../../assets/emptystar.png";
 export default function ReviewCard() {
   const ReviewsData = [
     {
-      pimg: profileImg,
+      Image: profileImg,
       alt: "prfileImg",
       name: "hihihi",
       tags: ["#줌 스터디", "#공부", "#카페"],
-      average: 5,
-      review:
+      score: 5,
+      content:
         "이런 서비스를 쓸 때 어떤 사람들을 만나게 될지 몰라서 처음에는 솔직히 걱정스러웠지만 이젠 부담감 없이 사용할 수 있어요.",
     },
   ];
@@ -21,14 +21,14 @@ export default function ReviewCard() {
   const [Slideindex, setSlideindex] = useState(0);
 
   const ratingToPercent = {
-    width: `${ReviewsData[Slideindex].average * 15.5 + 5}%`,
+    width: `${ReviewsData[Slideindex].score * 15.5 + 5}%`,
   };
   return (
     <div className="ReviewBox">
       <div className="Contents">
         <div className="UserBox">
           <img
-            src={ReviewsData[Slideindex].pimg}
+            src={ReviewsData[Slideindex].Image}
             alt={ReviewsData[Slideindex].alt}
           />
           <div className="UserDetailBox">
@@ -55,11 +55,11 @@ export default function ReviewCard() {
                 <img src={emptyStarImg} alt="emptystar" />
                 <img src={emptyStarImg} alt="emptystar" />
               </div>
-              <div className="Point">{ReviewsData[Slideindex].average}</div>
+              <div className="Point">{ReviewsData[Slideindex].score}</div>
             </div>
           </div>
         </div>
-        <div className="UserReviewBox">{ReviewsData[Slideindex].review}</div>
+        <div className="UserReviewBox">{ReviewsData[Slideindex].content}</div>
       </div>
     </div>
   );
