56c5e4e87b5b3d66bf8610e8caeb408b7dd9567b
choihj <choihj0510@naver.com>
2024-02-14 22:35:49 +0900

    #203 fix : "로그인 - 현진"

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,13 +2,17 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setUserEmail, setUserPassword } from "../store/LoginActions";
-import { Link, useNavigate } from 'react-router-dom';
-import googleIcon from '../assets/google-icon.png';
-import naverIcon from '../assets/naver-icon.png';
-import kakaoIcon from '../assets/kakao-icon.png';
-import LoginBanner from '../assets/login.png'
-import '../style/Login.css';
-import { GoogleLoginButton, KakaoLoginButton, NaverLoginButton } from "../util/SocialLogin";
+import { Link, useNavigate } from "react-router-dom";
+import googleIcon from "../assets/google-icon.png";
+import naverIcon from "../assets/naver-icon.png";
+import kakaoIcon from "../assets/kakao-icon.png";
+import LoginBanner from "../assets/login.png";
+import "../style/Login.css";
+import {
+  GoogleLoginButton,
+  KakaoLoginButton,
+  NaverLoginButton,
+} from "../util/SocialLogin";
 import { setIsLoggedIn } from "../store/LoginReducer";
 
 export default function Login() {
@@ -20,7 +24,7 @@ export default function Login() {
   const [keepLoggedIn, setKeepLoggedIn] = useState(false);
 
   useEffect(() => {
-    const storedIsLoggedIn = localStorage.getItem('isLoggedIn');
+    const storedIsLoggedIn = localStorage.getItem("isLoggedIn");
     if (storedIsLoggedIn) {
       setIsLoggedIn(true);
     }
@@ -39,18 +43,18 @@ export default function Login() {
   };
 
   const onClickConfirmButton = async (e) => {
-    if (email === '' || pw === '') {
-      alert('이메일과 비밀번호를 모두 입력해주세요.');
+    if (email === "" || pw === "") {
+      alert("이메일과 비밀번호를 모두 입력해주세요.");
       return;
     }
 
     try {
       setIsLoading(true);
 
-      // const res = await axios.post("https://hyunjin.link/api/auth/login", {
-      const res = await axios.post("https://hyeonjo.shop/api/auth/login", {
+      const res = await axios.post("https://hyunjin.link/api/auth/login", {
+        //const res = await axios.post("https://hyeonjo.shop/api/auth/login", {
         email: email,
-        password: pw
+        password: pw,
       });
       if (res.data.isSuccess) {
         setTimeout(() => {
@@ -58,13 +62,14 @@ export default function Login() {
 
           setIsLoading(false);
           alert("로그인에 성공했습니다.");
-          localStorage.setItem('token', token);
+          localStorage.setItem("token", token);
           // setAuthHeader(token);
           dispatch(setIsLoggedIn(true));
-          navigate('/');
+          navigate("/");
 
-          if (keepLoggedIn) { // 로그인 상태 유지
-            localStorage.setItem('isLoggedIn', 'true');
+          if (keepLoggedIn) {
+            // 로그인 상태 유지
+            localStorage.setItem("isLoggedIn", "true");
             dispatch(setIsLoggedIn(true));
           }
         }, 1500);
@@ -83,25 +88,22 @@ export default function Login() {
       //     dispatch(setIsLoggedIn(true));
       //   }
       // }, 1500);
-    }
-    catch (err) {
+    } catch (err) {
       alert(err);
       setIsLoading(false);
     }
 
-    dispatch(setUserEmail(''));
-    dispatch(setUserPassword(''));
+    dispatch(setUserEmail(""));
+    dispatch(setUserPassword(""));
   };
 
   return (
     <div className="login-page">
-
       <div className="banner-image-container">
         <img className="banner-image" src={LoginBanner} alt="Login img" />
       </div>
 
       <div className="login-content-wrap">
-
         <div className="login-title-wrap">로그인</div>
 
         <div className="login-input-title">이메일</div>
@@ -136,13 +138,23 @@ export default function Login() {
         </div>
 
         <div>
-          <button onClick={onClickConfirmButton} className="login-bottom-button" disabled={isLoading}>
-            {isLoading ? 'Loading...' : '로그인'}
+          <button
+            onClick={onClickConfirmButton}
+            className="login-bottom-button"
+            disabled={isLoading}
+          >
+            {isLoading ? "Loading..." : "로그인"}
           </button>
           <div className="additional-links">
-            <span><a href="/findEmail">계정 찾기</a> | </span>
-            <span><a href="/findPassword">비밀번호 찾기</a> | </span>
-            <span><a href="/signup">회원가입</a></span>
+            <span>
+              <a href="/findEmail">계정 찾기</a> |{" "}
+            </span>
+            <span>
+              <a href="/findPassword">비밀번호 찾기</a> |{" "}
+            </span>
+            <span>
+              <a href="/signup">회원가입</a>
+            </span>
           </div>
         </div>
 
@@ -159,6 +171,5 @@ export default function Login() {
         </div>
       </div>
     </div>
-
   );
 }
