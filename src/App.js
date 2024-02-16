bdfd13e80e9a01ff299266e6d6ac9fac6d07d460
jeondain <dain.jeon03@gmail.com>
2024-02-17 02:26:48 +0900

    Fix#107: merge 오류 수정

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import store from './store/LoginStore';
 import Category from './pages/Category';
 import Chatting from './pages/Chatting';
-import Friend from './pages/Friend';
 import NotFound from './pages/NotFound';
 import Posting from './pages/Posting';
 import Home from './pages/Home';
@@ -12,6 +11,11 @@ import Header from './components/Header';
 import MyPage from './pages/MyPage';
 import Login from './pages/Login';
 import Notification from './pages/Notification';
+import FriendProfile from "./pages/FriendProfile";
+import FriendList from "./pages/FriendList";
+import FriendFollower from "./pages/FriendFollower";
+import FriendFollowing from "./pages/FriendFollowing";
+import FriendMatching from "./pages/FriendMatching";
 import PostUser from './pages/PostUser';
 import SignUp from './pages/SignUp';
 import MatchingSend from './pages/MatchingSend';
@@ -26,25 +30,41 @@ function App() {
   return (
     <div className="root-wrap">
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element= {<Home/>} />
-        <Route path="/category" element= {<Category/>} />
-        <Route path="/chatting" element= {<Chatting/>} />
-        <Route path="/friend" element= {<Friend/>} />
-        <Route path="/follower" element={<FriendFollower />} />
-          <Route path="/following" element={<FriendFollowing />} />
-          <Route path="/matching" element={<FriendMatching />} />
-          <Route path="/FriendProfile" element={<FriendProfile />} />
-        <Route path="/posting" element= {<Posting/>} />
-        <Route path="/mypage" element= {<MyPage/>} />
-        <Route path="/notification" element= {<Notification/>} />
-        <Route path="/login" element= {<Login/>} />
-        <Route path="/*" element= {<NotFound/>} />
-      </Routes>
-      <Footer />
-
-    </BrowserRouter>
+        <Provider store={store}>
+          <Routes>
+            <Route
+              path="/*"
+              element={
+                <div>
+                  <Header />
+                  <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/category" element={<Category />} />
+                    <Route path="/chatting" element={<Chatting />} />
+                    <Route path="/posting" element={<Posting />} />
+                    <Route path="/postuser/:id" element={<PostUser />} />
+                    <Route path="/mypage" element={<MyPage />} />
+                    <Route path="/notification" element={<Notification />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/findEmail" element={<FindEmail />} />
+                    <Route path="/findPassword" element={<FindPassword />} />
+                    <Route path="/signup" element={<SignUp />} />
+                    <Route path="/authentication" element={<SignUpAuthentication />} />
+                    <Route path="/checkSms" element={<SignUpAuthenticationCheck />} />
+                    <Route path="/matching" element={<MatchingSend />} />
+                    <Route path="/friend" element={<FriendList />} />
+                    <Route path="/follower" element={<FriendFollower />} />
+                    <Route path="/following" element={<FriendFollowing />} />
+                    <Route path="/FriendProfile" element={<FriendProfile />} />
+                    <Route path="/*" element={<NotFound />} />
+                  </Routes>
+                  {/* <Footer /> */}
+                </div>
+              }
+            />
+          </Routes>
+        </Provider>
+      </BrowserRouter>
     </div>
   );
 }
